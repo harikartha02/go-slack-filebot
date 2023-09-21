16542f17b41d9f18da8e06205cb7654f7c5830c6
harikartha02 <95367818+harikartha02@users.noreply.github.com>
2023-09-21 11:56:44 +0530

    Committed full project

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-5923270249730-5939144975697-s6F6ksDXxUT8quXrXBrjrAMn")
+	os.Setenv("CHANNEL_ID", "C05T7M8MXMJ")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{"Resume.pdf"}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL: %s\n", file.Name, file.URL)
+	}
+}
